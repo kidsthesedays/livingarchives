f456ebc79ed32b1c99c0f5300740fc833125c0c7
Sebastian Bengtegård <sebbebook@gmail.com>
2016-11-07 12:41:01 +0100

    updated gitignore with new rules

--- node/static/bundle.js
+++ node/static/bundle.js
@@ -56,8 +56,35 @@
 	console.log('Hello');
 
 	for (var i = 0, x = 0; i > 5; i++) {
-	    console.log(i, x);
+	  console.log(i, x);
 	}
+
+	/**
+	 * @function _guid
+	 * @description Creates GUID for user based on several different browser variables
+	 * It will never be RFC4122 compliant but it is robust
+	 * @returns {Number}
+	 * @private
+	 **/
+	// var guid = function() {
+	// 
+	//     var nav = window.navigator;
+	//     var screen = window.screen;
+	//     var guid = nav.mimeTypes.length;
+	//     guid += nav.userAgent.replace(/\D+/g, '');
+	//     guid += nav.plugins.length;
+	//     guid += screen.height || '';
+	//     guid += screen.width || '';
+	//     guid += screen.pixelDepth || '';
+	// 
+	//     return guid;
+	// };
+	// var doNotTrack = function () {
+	// 
+	//     if (!window.navigator.userAgent.match(/MSIE\s10\.0|trident\/6\.0/i)) {
+	//         return window.navigator.doNotTrack || window.navigator.msDoNotTrack;
+	//     }
+	// };
 
 /***/ }
 /******/ ]);
