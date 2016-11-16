9cfaf4607dac18fbe300a00e46a42321c8139223
Sebastian Bengtegård <sebbebook@gmail.com>
2016-11-16 15:20:39 +0100

    added pm2 restarts on file changes

--- node/frontend-server.js
+++ node/frontend-server.js
@@ -2,9 +2,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+
 // Logger
 app.use(morgan('combined'))
 // Send index.html on all requests
 app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
+
 // Start listening for requests
 app.listen(3000, () => console.log('[FRONTEND SERVER] Started listening on port 3000'))
