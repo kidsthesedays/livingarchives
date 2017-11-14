3a7c7db8a9300f24f2e3bb30393600e99534f2cd
Sebastian Bengtegård <sebbebook@gmail.com>
2017-11-14 17:09:57 +0100

    current

--- sites/affexity.livingarchives.org/js/app.js
+++ sites/affexity.livingarchives.org/js/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const DEBUG = true;
-
-var VIDEOS = [
+const DATASET_URL = '/dataset/Affexity.xml';
+const VIDEOS = [
     {
         key: 'delicatepassage2',
         src: 'del-pass-red-wall-no-music.3gp',
@@ -17,10 +17,6 @@ var VIDEOS = [
     }
 ];
 
-const DATASET_URL = '/dataset/Affexity.xml';
-
-let animationIsPending = false;
-
 let Video = {
     src: '', 
     elem: document.createElement('video')
@@ -28,6 +24,8 @@ let Video = {
 
 Video.elem.setAttribute('playsinline', '');
 enableInlineVideo(Video.elem);
+
+let animationIsPending = false;
 
 // Initialize Argon, the Scene, the Camera and the WebGL Renderer
 const App = Argon.init();
@@ -89,6 +87,7 @@ const shaderMaterial = new THREE.ShaderMaterial({
     `
 });
 
+// TODO does the dimensions need to be dynamic?
 const videoGeometry = new THREE.PlaneGeometry(500, 500);
 const videoMesh = new THREE.Mesh(videoGeometry, shaderMaterial);
 
@@ -142,19 +141,10 @@ const setupImageTracking = v => () => {
         if (DEBUG) console.log('FOUND video', v.src);
 
         loadVideo(Video, v.src)
-
-        if (Video.src !== '' && Video.src !== v.src) {
-            window.setTimeout(() => {
-                v.object.add(videoObject);
-                videoObject.position.z = 0
-            }, 350);
-        } else {
-            v.object.add(videoObject);
-            videoObject.position.z = 0;
-        }
+        v.object.add(videoObject);
+        videoObject.position.z = 0
 
         document.addEventListener('touchstart', handleTouchStart, false);
-        // document.addEventListener('click', handleTouchStart, false);
     // LOST
     } else if (vp.poseStatus & Argon.PoseStatus.LOST) {
         videoObject.position.z = -0.5;
@@ -162,7 +152,6 @@ const setupImageTracking = v => () => {
         Video.elem.pause();
 
         document.removeEventListener('touchstart', handleTouchStart, false);
-        // document.removeEventListener('click', handleTouchStart, false);
     }
 };
 
@@ -256,285 +245,3 @@ const renderEventFn = () => {
 App.vuforia.isAvailable().then(setupVuforia);
 App.context.updateEvent.addEventListener(updateEventFn);
 App.context.renderEvent.addEventListener(renderEventFn);
-
-// TODO: add id-tracking of which video is playing so we can switch when a new one comes along
-// TODO: rewrite to es6
-// TODO: check which event handler is needed: click or touch
-// TODO: add all videos to this file
-// TODO: add DEBUG constant for easier debugging
-
-// Enables click, written inside the "handleEvent" function of event forwarder
-// SEBBE
-// if (e.type === 'click') return;
-// if (e.type === 'touchstart') return;
-
-// var DATASET_URL = '/dataset/Affexity.xml';
-// 
-// // Control variables
-// var animationPending = false;
-// 
-// // Initialize Argon, the scene and the camera (THREE.js)
-// var app = Argon.init();
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera();
-// var userLocation = new THREE.Object3D();
-// var renderer = new THREE.WebGLRenderer({
-//     alpha: true,
-//     logarithmicDepthBuffer: true,
-//     antialias: Argon.suggestedWebGLContextAntialiasAttribute
-// });
-// 
-// // Add the camera and user to the scene
-// scene.add(camera);
-// scene.add(userLocation);
-// 
-// renderer.setPixelRatio(window.devicePixelRatio);
-// 
-// app.view.setLayers([{ source: renderer.domElement }]);
-// 
-// // Object that will contain our video in 3d-space
-// var argonVideoObject = new THREE.Object3D();
-// // Hide it by default
-// argonVideoObject.position.z = -0.5;
-// argonVideoObject.scale.set(0.001, 0.001, 0.001);
-// // Add it to the user so we can render it infront of the user
-// userLocation.add(argonVideoObject);
-// 
-// // <video> element used to play videos
-// var videoElement = document.createElement('video');
-// videoElement.setAttribute('playsinline', '');
-// // Disable fullscreen videos
-// // TODO: needed?
-// enableInlineVideo(videoElement);
-// 
-// // The canvas in which we render the video
-// // var videoCanvas = document.createElement('canvas');
-// // videoCanvas.width = 640;
-// // videoCanvas.height = 360;
-// 
-// // 
-// // var videoCanvasContext = videoCanvas.getContext('2d');
-// // videoCanvasContext.fillStyle = '#000000';
-// // videoCanvasContext.fillRect(0, 0, videoCanvas.width, videoCanvas.height);
-// 
-// var videoTexture = new THREE.VideoTexture(videoElement);
-// videoTexture.minFilter = THREE.LinearFilter;
-// videoTexture.magFilter = THREE.LinearFilter;
-// 
-// 
-// // var videoTexture = new THREE.Texture(videoCanvas);
-// // videoTexture.minFilter = THREE.LinearFilter;
-// // videoTexture.magFilter = THREE.LinearFilter;
-// 
-// var shaderMaterial = new THREE.ShaderMaterial({
-//     uniforms: {
-//         texture: {
-//             type: 't',
-//             value: videoTexture
-//         }
-//     },
-//     vertexShader: [
-//         'varying vec2 vUv;',
-//         'varying float texU;',
-//         'void main()',
-//         '{',
-//         'vUv = uv;',
-//         'vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
-//         'gl_Position = projectionMatrix * mvPosition;',
-//         '}'
-//     ].join('\n'),
-// 
-//     fragmentShader: [
-//         'uniform sampler2D texture;',
-//         'uniform vec3 color;',
-//         'varying vec2 vUv;',
-//         'void main()',
-//         '{',
-//         'vec2 texcoord = vec2(0.49, 0.0);',
-//         'vec2 halfTex = vec2(0.5, 1.0);',
-//         'vec3 tColor = texture2D( texture, ( vUv * halfTex ) ).rgb;',
-//         'vec3 aColor = texture2D( texture, ( (vUv * halfTex ) + texcoord ) ).rgb;',
-//         'float a = aColor.g;',
-//         'gl_FragColor = vec4(tColor, a);',
-//         '}'
-//     ].join('\n')
-// });
-// 
-// var videoGeometry = new THREE.PlaneGeometry(500, 500); // TODO does this need to be dynamic?
-// 
-// // var videoMaterial = new THREE.MeshBasicMaterial({
-// //     shader: shaderMaterial,
-// //     map: videoTexture,
-// //     // overdraw: true,
-// //     // transparent: true,
-// //     // opacity: 0.5
-// // });
-// 
-// // var videoScreen = new THREE.Mesh(videoGeometry, videoMaterial);
-// var videoScreen = new THREE.Mesh(videoGeometry, shaderMaterial);
-// 
-// argonVideoObject.add(videoScreen);
-// 
-// function handleTouchStart(e) {
-//     if (e.touches === undefined || e.touches.length !== 1) {
-//         return
-//     }
-// 
-//     console.log('TOUCHED!');
-// 
-//     e.preventDefault();
-// 
-//     if (videoElement.paused) {
-//         videoElement.play();
-//     } else {
-//         videoElement.pause();
-//     }
-// }
-// 
-// function imageTrackingFn(v) {
-//     return function() {
-// 
-//         var videoPose = app.context.getEntityPose(v.entity);
-// 
-//         // KNOWN
-//         if (videoPose.poseStatus & Argon.PoseStatus.KNOWN) {
-//             v.object.position.copy(videoPose.position);
-//             v.object.quaternion.copy(videoPose.orientation);
-//         }
-// 
-//         // FOUND
-//         if (videoPose.poseStatus & Argon.PoseStatus.FOUND) {
-//             v.object.add(argonVideoObject);
-//             argonVideoObject.position.z = 0;
-//             videoElement.src = '/videos/' + v.src;
-//             // TODO videoElement.loop = true;
-//             videoElement.load();
-//             // TODO settimeout -> videoElement.play()?
-// 
-//             // TODO might have to fix 'touchstart' within the Argon source?
-//             document.addEventListener('touchstart', handleTouchStart, false);
-//             document.addEventListener('click', handleTouchStart, false);
-//         // LOST
-//         } else if (videoPose.poseStatus & Argon.PoseStatus.LOST) {
-//             argonVideoObject.position.z = -0.5;
-//             userLocation.add(argonVideoObject);
-//             videoElement.pause();
-// 
-//             // TODO might have to fix 'touchstart' within the Argon source?
-//             document.removeEventListener('touchstart', handleTouchStart, false);
-//             document.removeEventListener('click', handleTouchStart, false);
-//         }
-//     }
-// }
-// 
-// function setupTrackables(trackables) {
-//     console.log('WILL SETUP TRACKABLES: ', VIDEOS.length);
-// 
-//     // Videos from the global scope
-//     VIDEOS.map(function(v) {
-//         app.context
-//             .subscribe(trackables[v.key].id)
-//             .then(function(entity) {
-// 
-//                 var e = Object.assign({}, v, {
-//                     entity: entity,
-//                     object: new THREE.Object3D()
-//                 });
-// 
-//                 scene.add(e.object);
-// 
-//                 var trackingFn = imageTrackingFn(e);
-// 
-//                 app.context.updateEvent.addEventListener(trackingFn);
-//             });
-//     });
-// }
-// 
-// // Initialize and setup vuforia
-// function setupVuforia(available) {
-//     if (!available) {
-//         console.log('Vuforia is not available on this platform');
-//         return;
-//     }
-// 
-//     app.vuforia
-//         .init({ encryptedLicenseData: LICENSE }) // License from the global scope
-//         .then(function(api) {
-// 
-//             console.log('LICENSE VALID!')
-// 
-//             // Create the dataset from our URL
-//             api.objectTracker
-//                 .createDataSetFromURL(DATASET_URL)
-//                 .then(function(dataset) {
-//                     // Load the dataset
-//                     api.objectTracker
-//                         .loadDataSet(dataset)
-//                         .then(setupTrackables);
-// 
-//                     // Activate our dataset
-//                     api.objectTracker
-//                         .activateDataSet(dataset);
-//                 });
-//         })
-//         .catch(function(e) { console.log('Vuforia was unable to initialize') });
-// }
-// 
-// function updateEventFn() {
-//     var userPose = app.context.getEntityPose(app.context.user);
-// 
-//     if (userPose.poseStatus & Argon.PoseStatus.KNOWN) {
-//         userLocation.position.copy(userPose.position);
-//     }
-// }
-// 
-// function renderFunc() {
-//     animationPending = false;
-// 
-//     renderer.setSize(app.view.renderWidth, app.view.renderHeight, false);
-//     renderer.setPixelRatio(app.suggestedPixelRatio);
-// 
-//     for (var i = 0, sv = app.view.subviews; i < sv.length; i++) {
-//         var subview = sv[i];
-// 
-//         camera.position.copy(subview.pose.position);
-//         camera.quaternion.copy(subview.pose.orientation);
-//         camera.projectionMatrix.fromArray(subview.projectionMatrix);
-// 
-//         var x = subview.viewport.x;
-//         var y = subview.viewport.y;
-//         var width = subview.viewport.width;
-//         var height = subview.viewport.height;
-// 
-//         if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
-//             // videoCanvasContext.drawImage(
-//             //     videoElement,
-//             //     0,
-//             //     0,
-//             //     videoCanvas.width,
-//             //     videoCanvas.height
-//             // );
-// 
-//             if (videoTexture) {
-//                 videoTexture.needsUpdate = true;
-//             }
-//         }
-// 
-//         renderer.setViewport(x, y, width, height);
-//         renderer.setScissor(x, y, width, height);
-//         renderer.setScissorTest(true);
-//         renderer.render(scene, camera, subview.index);
-//     }
-// }
-// 
-// function renderEventFn() {
-//     if (!animationPending) {
-//         animationPending = true;
-//         window.requestAnimationFrame(renderFunc);
-//     }
-// }
-// 
-// // Lets go!
-// app.vuforia.isAvailable().then(setupVuforia); // TODO add catch?
-// app.context.updateEvent.addEventListener(updateEventFn);
-// app.context.renderEvent.addEventListener(renderEventFn);
